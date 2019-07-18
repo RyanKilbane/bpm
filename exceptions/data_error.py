cb2e9df2714c1233749183fae895213955bb5867
RyanKilbane <RyanKilbane92@hotmail.com>
2019-07-18 01:12:19 +0100

    Added exception for missing or extraneous ingest data, added some basic functionality for POSTing json

---
+++ exceptions/data_error.py
@@ -0,0 +1,3 @@
+class DataError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,7 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, request
+import json
+from uuid import uuid4
+from db_interface.get_metadata import Metadata
+from exceptions.data_error import DataError
 
 ingest_point = Blueprint('ingest_point', __name__)
 
 @ingest_point.route('/', methods=["POST"])
 def landing():
-    pass
+    table_metadata = Metadata(test_env=True, table_name="ingest", db_name="bpm_test")
+    table_metadata.make_engine()
+    table_data = table_metadata.get_table_data()
+    data = request.get_json()
+    try:
+        check(table_data, data, "bpm_id")
+    except DataError as data_error:
+        return "OH NO! An error\n\n{}\n".format(data_error)
+    
+    data_with_id = assign_uuid(data)
+
+    return data_with_id
+
+def check(table_metadata, ingest_data, *ignore):
+    column_names = [column["name"] for column in table_metadata]
+    for attribute in ingest_data["id_info"].keys():
+        if attribute not in column_names:
+            raise DataError("Ingest data contains too many attributes!\nExpecting: {}\nBut got: {}".format(column_names, ingest_data["id_info"].keys()))
+    
+    for col in column_names:
+        if col not in ingest_data["id_info"].keys() and col not in ignore:
+            raise DataError("Ingest data contains too few attributes!\nExpecting: {}\nBut got: {}".format(column_names, ingest_data["id_info"].keys()))
+
+def assign_uuid(ingest_data):
+    ingest_data["id_info"]["bpm_id"] = str(uuid4())
+    return ingest_data
+    
