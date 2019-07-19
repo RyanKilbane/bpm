c667d4c85cbd6563a0963a005f3e900620984edb
RyanKilbane <RyanKilbane92@hotmail.com>
2019-07-19 12:09:41 +0100

    Started using Axios to do a get on the allocate API

--- allocation-ui/src/App.js
+++ allocation-ui/src/App.js
@@ -6,24 +6,7 @@ import axios from 'axios';
 function App() {
   var x = axios.get("http://localhost:5000/allocate")
   x.then(response => console.log(response.data))
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
-  );
+  return x.then(response => response.data)
 }
 
 export default App;
